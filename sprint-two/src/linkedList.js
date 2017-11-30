97bb6d1cb54dc7c1447b79266ef70a8479208563
iyyu <iyucomms@gmail.com>
2017-11-30 10:30:16 -0800

    addToTail completed for linkedList

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -1,9 +1,14 @@
 var LinkedList = function() {
   var list = {};
   list.head = null;
-  list.tail = null;
+  list.tail = null; 
 
   list.addToTail = function(value) {
+    let node = Node(value);
+    if (list.tail !== null) {
+      list.tail.next = node;
+    }
+    list.tail = node;  
   };
 
   list.removeHead = function() {
@@ -26,4 +31,7 @@ var Node = function(value) {
 
 /*
  * Complexity: What is the time complexity of the above functions?
+addToTail: O(n)
+removeHead O(1)
+contains O(n)
  */
