cbebaf03ac89d0fcfb15ff67bd4d6644db30fb9f
iyyu <iyucomms@gmail.com>
2017-12-01 11:40:15 -0800

    WIP HashTable, working insert and retrieve methods

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -1,16 +1,41 @@
+  // limitedArray.set = function(index, value) {
+  //   checkLimit(index);
+  //   storage[index] = value;
+  // };
 
+// limitedArray.get = function(index) {
+//     checkLimit(index);
+//     return storage[index];
+//   };
 
 var HashTable = function() {
   this._limit = 8;
   this._storage = LimitedArray(this._limit);
+  
 };
 
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  // debugger;
+  // let bucket = this._storage[index] || [];
+  // if (this._storage[index] === undefined) {
+  //   this._storage.set(index, []);
+  //   console.log('type test ', typeof this._storage);
+  //   this._storage[index].push([k, v]);
+  // }
+  // this._storage[index].concat([[k, v]]);
+  this._storage.set(index, [[k, v]]);
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  // let bucket = this._storage[index];
+  // for (let i = 0; i < bucket.length; i++) {
+  //   if (bucket[i][0] === k) {
+  //     return bucket[i][1];
+  //   }
+  // }
+  return this._storage.get(index)[0][1];
 };
 
 HashTable.prototype.remove = function(k) {
@@ -24,3 +49,5 @@ HashTable.prototype.remove = function(k) {
  */
 
 
+let hash = new HashTable();
+hash._storage
