11f0df0318c9c78cfa24281324b244c64abe8ff4
iyyu <iyucomms@gmail.com>
2017-12-01 19:30:32 -0800

    WIP Setw

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,18 +1,38 @@
 var Set = function() {
   var set = Object.create(setPrototype);
-  set._storage = null; // fix me
+  set._storage = []; // fix me
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item) {
+  // if the storage does not contain item
+    // push item into the 
+  if (this._storage.contains(item === false)) {
+    this._storage.push(item);
+  }
 };
 
 setPrototype.contains = function(item) {
+  let found;
+  
+  if (this._storage.indexOf(item) === -1) {
+    found = false;
+  } else {
+    found = true;
+  }
+  return found;
 };
 
 setPrototype.remove = function(item) {
+  // find item index
+    // if index = -1 return 'not found'
+  // else
+    // remove index from set (using splice if in JS)
+  
+  let index = this._storage.indexOf(item);
+  
 };
 
 /*
